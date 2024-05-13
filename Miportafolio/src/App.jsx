72212c6bf0377ca1d5be456553690212729ac254
Valeria-Ramirez130 <116677886+Valeria-Ramirez130@users.noreply.github.com>
2024-05-12 23:13:08 -0500

    NavBar, inicio, mis habilidades,contacto listo

--- Miportafolio/src/App.jsx
+++ Miportafolio/src/App.jsx
@@ -1,35 +1,116 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import NavBar from './components/NavBar';
+import "./App.css";
+import './fuente.css';
+import fondo from './img/fondo.jpg'; // Cambia el nombre del archivo según sea necesario
+import foto from './img/FotoCV.jpg';
+import logohtml from './img/logohtml.svg';
+import logocss from './img/CSS-Logo.svg';
+import logoreact from './img/Reactlogo.svg';
+import logobootstrap from './img/Bootstrap_logo.svg';
+import logogithub from './img/Github-logo.svg';
+import JavaScript_logo from './img/JavaScript_logo.svg';
+import iconoCopy from './img/iconocopy.svg';
+import iconoemail from './img/iconoemail.svg';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const handleCopyEmail = () => {
+    const inputEmail = document.getElementById('exampleInputEmail1');
+    // Copiar el contenido del input al portapapeles
+    inputEmail.select();
+    document.execCommand('copy');
+    // Deseleccionar el texto para evitar resaltarlo
+    window.getSelection().removeAllRanges();
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <header className='Header'>
+        <NavBar />
+      </header>
+      <div className='contenedor-principal'>
+        <section id="inicio" style={{ backgroundImage: `url(${fondo})` }}>
+          <div className="circle">
+            <img src={foto} alt="FotoCV" className="image" />
+          </div>
+          <div className='secciones seccion-inicio'>
+            <div className="presentacion">
+              <h3 className='presentacion saludo'>¡Hola!</h3>
+              <h1 className='presentacion nombre'>Soy Valeria Ramirez</h1>
+              <h3 className='presentacion profesion'>Desarolladora Front-end</h3>
+            </div>
+          </div>
+        </section>
+        {/* Resto de las secciones sin fondo */}
+        <section id="acerca-de-mi">
+          <div className='secciones seccion-acerca-de-mi'>
+            <h2><strong>Acerca de mi</strong></h2>
+            <h5>Soy Valeria Ramirez / Desarrolladora Frontend / Tecnologa en desarrollo de software</h5>
+            <p>Soy una Desarrolladora Front-End Junior con habilidades en HTML, CSS y JavaScript. Ademas
+               Diseñar y desarrollar interfaces atractivas y funcionales utilizando tecnologías como Bootstrap y React. 
+              Apasionada por aprender y contribuir en proyectos innovadores, 
+              estoy comprometida con el crecimiento continuo y la excelencia en mi trabajo.</p>
+          </div>
+        </section>
+
+        <section id="mis-habilidades">
+          <div className='secciones seccion-mis-habilidades'>
+            <h2><strong>Mis Habilidades</strong></h2>
+            <div className="logo-container">
+              <img src={logohtml} alt="HTML" className="logo" />
+              <span>HTML</span>
+            </div>
+            <div className="logo-container">
+              <img src={logocss} alt="CSS" className="logo" />
+              <span>CSS</span>
+            </div>
+            <div className="logo-container">
+              <img src={JavaScript_logo} alt="JavaScript" className="logo" />
+              <span>JavaScript</span>
+            </div>
+            <div className="logo-container">
+              <img src={logobootstrap} alt="Bootstrap" className="logo" />
+              <span>Bootstrap</span>
+            </div>
+            <div className="logo-container">
+              <img src={logoreact} alt="React" className="logo" />
+              <span>React</span>
+            </div>
+            <div className="logo-container">
+              <img src={logogithub} alt="GitHub" className="logo" />
+              <span>GitHub</span>
+            </div>
+          </div>
+        </section>
+
+        <section id="proyectos">
+          <div className='secciones seccion-proyectos'>
+            <h2><strong>Proyectos</strong></h2>
+            <p>ESTOY EN PROYECTOS</p>
+            <p>ESTOY EN PROYECTOS</p>
+            <p>ESTOY EN PROYECTOS</p>
+            <p>ESTOY EN PROYECTOS</p>
+            <p>ESTOY EN PROYECTOS</p>
+            <p>ESTOY EN PROYECTOS</p>
+            <p>ESTOY EN PROYECTOS</p>
+            <p>ESTOY EN PROYECTOS</p>
+          </div>
+        </section>
+
+        <section id="contacto">
+          <div className='secciones seccion-contacto'>
+            <h2><strong>Contacto</strong></h2>
+            <div className="input-container">
+              <img src={iconoemail} alt="Icono Email" className="icono-email" />
+              <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value="valeriaramirezmarin52@gmail.com" readOnly />
+              <img src={iconoCopy} alt="Icono Copy" className="icono-copy" id="iconoCopy" onClick={handleCopyEmail} />
+            </div>
+          </div>
+        </section>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ Miportafolio/src/components/NavBar.jsx
@@ -0,0 +1,98 @@
+import React, { useState, useEffect } from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import "./NavBar.css";
+
+export function NavBar() {
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const sections = document.querySelectorAll('section');
+      let selectedSectionId = null;
+
+      sections.forEach(section => {
+        const rect = section.getBoundingClientRect();
+        if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+          selectedSectionId = section.id;
+        }
+      });
+
+      setSelectedOption(selectedSectionId);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const handleNavLinkClick = (event, targetId) => {
+    event.preventDefault();
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) {
+      window.scrollTo({
+        top: targetElement.offsetTop - 100,
+        behavior: "smooth"
+      });
+    }
+
+    // Solo actualiza el estado si la opción seleccionada es diferente
+    if (selectedOption !== targetId) {
+      setSelectedOption(targetId);
+    }
+  };
+
+  return (
+    <Navbar expand="lg" className="custom-navbar">
+      <Container>
+        <Navbar.Brand href="#home">
+          <span className="brand-text">{"<VR/>"}</span> {/* Texto <VR/> */}
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" className="custom-toggler" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link 
+              onClick={(e) => handleNavLinkClick(e, 'inicio')} 
+              href="#inicio" 
+              className={selectedOption === 'inicio' ? 'selected' : ''}
+            >
+              Inicio
+            </Nav.Link>
+            <Nav.Link 
+              onClick={(e) => handleNavLinkClick(e, 'acerca-de-mi')} 
+              href="#acerca-de-mi" 
+              className={selectedOption === 'acerca-de-mi' ? 'selected' : ''}
+            >
+              Acerca de mi
+            </Nav.Link>
+            <Nav.Link 
+              onClick={(e) => handleNavLinkClick(e, 'mis-habilidades')} 
+              href="#mis-habilidades" 
+              className={selectedOption === 'mis-habilidades' ? 'selected' : ''}
+            >
+              Mis Habilidades
+            </Nav.Link>
+            <Nav.Link 
+              onClick={(e) => handleNavLinkClick(e, 'proyectos')} 
+              href="#proyectos" 
+              className={selectedOption === 'proyectos' ? 'selected' : ''}
+            >
+              Proyectos
+            </Nav.Link>
+            <Nav.Link 
+              onClick={(e) => handleNavLinkClick(e, 'contacto')} 
+              href="#contacto" 
+              className={selectedOption === 'contacto' ? 'selected' : ''}
+            >
+              Contacto
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBar;
